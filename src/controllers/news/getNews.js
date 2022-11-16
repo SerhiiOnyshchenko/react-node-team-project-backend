52ed9a7a8050523826d902fac7e10385bb0daa92
Natalia Monasturska <tjomamon@gmail.com>
2022-11-16 17:34:28 +0200

    implements a search by query

--- src/controllers/news/getNews.js
+++ src/controllers/news/getNews.js
@@ -3,14 +3,22 @@ const News = require("../../db/news");
 const getNews = async (req, res) => {
     // without pagination
 
-    const result = await News.find();
+    // const result = await News.find();
 
     // with pagination
-    // const { page = 1, limit = 6 } = req.query;
-    // const skip = (page - 1) * limit;
+    const { page = 1, limit = 6, query } = req.query;
+    const skip = (page - 1) * limit;
+    let result = []
 
-    // const result = await News.find({}, null, { skip, limit: Number(limit) });
+    if (query) {
+          // тут сделать сортировку по дате
+        result = await News.find({ $or: [{ description: { $regex: `${query}` } }, { title: { $regex: `${query}` } }] }, null, { skip, limit: Number(limit) }).exec();
+    }
 
+    if (query === undefined) {
+        // тут сделать сортировку по дате
+        result = await News.find({}, null, { skip, limit: Number(limit) });
+    };
 
     res.json({
         status: "success",
