5dc658499b8a703822168ef7aa882eb19b3cdc28
Serhii Onyshchenko <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2022-11-18 15:42:49 +0200

    Update noticesModel.js

--- src/db/noticesModel.js
+++ src/db/noticesModel.js
@@ -1,53 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
 const noticesSchema = new mongoose.Schema({
-  titleOfAd: {
-    type: String,
-    required: true,
-  },
-  namePet: {
-    type: String,
-  },
+	titleOfAd: {
+		type: String,
+		required: true,
+	},
+	namePet: {
+		type: String,
+	},
 
-  dateOfBirth: {
-    type: String,
-  },
+	dateOfBirth: {
+		type: String,
+	},
 
-  breed: {
-    type: String,
-  },
+	breed: {
+		type: String,
+	},
 
-  sex: {
-    type: String,
-    enum: ["female", "male"],
-  },
+	sex: {
+		type: String,
+		enum: ['female', 'male'],
+	},
 
-  location: {
-    type: String,
-    required: true,
-  },
+	location: {
+		type: String,
+		required: true,
+	},
 
-  price: {
-    type: String,
-    required: true,
-  },
+	price: {
+		type: String,
+		required: true,
+	},
 
-  comments: {
-    type: String,
-  },
-
-<<<<<<< HEAD
-  category: {
-    type: String,
-    enum: ["lost/found", "in good hands", "sell"],
-  },
-  owner: {
-  	type: Schema.Types.ObjectId,
-  	ref: 'user',
-  },
-=======
-	image: {
+	comments: {
 		type: String,
 	},
 
@@ -55,15 +41,19 @@ const noticesSchema = new mongoose.Schema({
 		type: String,
 		enum: ['lost/found', 'in good hands', 'sell'],
 	},
+
 	owner: {
 		type: Schema.Types.ObjectId,
 		ref: 'user',
 	},
->>>>>>> 72f7e05ebf401e392d01e7e11c208dfe4335fe66
+
+	image: {
+		type: String,
+	},
 });
 
-const Notices = mongoose.model("notices", noticesSchema);
+const Notices = mongoose.model('notices', noticesSchema);
 
 module.exports = {
-  Notices,
+	Notices,
 };
