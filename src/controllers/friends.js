2ff74caaffb8c35838e70900a6c1644d6f3aa5b7
Kashyrets Ivanna <kasirecivanna@gmail.com>
2022-11-18 14:43:05 +0200

    unimportant

--- src/controllers/friends.js
+++ src/controllers/friends.js
@@ -1,14 +1,10 @@
-const { getAllFriends } = require("../services/friendsService");
+const { getAllFriends } = require('../services/friendsService');
 
 const getFriendsController = async (req, res) => {
-  const friends = await getAllFriends();
-  if (!friends) {
-    res.status(400);
-  }
-
-  res.status(200).json(friends);
+	const friends = await getAllFriends();
+	res.status(200).json(friends);
 };
 
 module.exports = {
-  getFriendsController,
+	getFriendsController,
 };
