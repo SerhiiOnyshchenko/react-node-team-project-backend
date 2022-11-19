b1e023053955d5e8a5b8e500e496f57c11bc5a3f
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-11-19 12:17:04 +0200

    return all fills  of cities, breeds

--- src/services/breedService.js
+++ src/services/breedService.js
@@ -4,9 +4,10 @@ const searchBreeds = async name => {
 	const breeds = await Breed.find({
 		breed: { $regex: name + '.*', $options: 'i' },
 	}).limit(20);
-	return breeds.map(({ breed }) => {
-		return { breed };
-	});
+	return breeds;
+	// return breeds.map(({ breed }) => {
+	// 	return { breed };
+	// });
 };
 
 module.exports = { searchBreeds };
--- src/services/cityService.js
+++ src/services/cityService.js
@@ -4,9 +4,10 @@ const searchCities = async name => {
 	const cities = await City.find({
 		city: { $regex: name + '.*', $options: 'i' },
 	}).limit(20);
-	return cities.map(({ city, region }) => {
-		return { city, region };
-	});
+	return cities;
+	// return cities.map(({ city, region }) => {
+	// 	return { city, region };
+	// });
 };
 
 module.exports = { searchCities };
