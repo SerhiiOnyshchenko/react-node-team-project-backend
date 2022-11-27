10a83983d7b859b40aa41b01dfaafc7348d23ce4
Сергій Онищенко <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2022-11-27 23:37:58 +0300

    Update noticesService.js

--- src/services/noticesService.js
+++ src/services/noticesService.js
@@ -5,7 +5,7 @@ const { s3Uploadv2 } = require('./s3service');
 const getNotices = async ({ category, q = "", page = 1, limit = 10 }) => {
 	const noticesToSkip = (page - 1) * limit;
 	const categoryText = category === 'in_good_hands' ? 'in good hands' : category
-	const noticesFilter = { titleOfAd: { $regex: q, $options: "i" }, categoryText };
+	const noticesFilter = { titleOfAd: { $regex: q, $options: "i" }, category: categoryText };
 	const noticesSort = { _id: -1 };
 
 	const notices = await Notices.find(noticesFilter).sort(noticesSort).skip(noticesToSkip).limit(limit);
